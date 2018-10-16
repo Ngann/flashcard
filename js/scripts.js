7bc9f067e55e3e81474d20e8757c8668b8b29086
Kristin Brewer-Lowe and Ngan Nguyen <kristin@>
2018-10-16 11:09:35 -0700

    Add html, css and js pages with basic functions

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  // $(".concept1").click(function() {
+  //   $(".answer1").toggle()
+  //
+  // });
+  //
+  // $(".concept2").click(function() {
+  //   $(".answer2").toggle()
+  // });
+  // $(".concept3").click(function() {
+  //   $(".answer3").toggle()
+  //
+  //
+  // });
+
+  $(".concept").click(function() {
+    $("this.nextElementSibling").toggle()
+  });
+
+
+});
