5699d209f19e7d11017d1fd73edba1a51d21b304
Kristin Brewer-Lowe and Ngan Nguyen <ngan@>
2018-10-16 15:16:35 -0700

    add content to answer.

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,36 @@
-// $(document).ready(function() {
+$(document).ready(function() {
+  $(".flashcard").click(function() {
+    $(this).children("p").slideToggle();
+    $(this).toggleClass("flashcard-clicked");
+  });
+});
+
 //   $(".concept").click(function() {
 //     $(this.nextElementSibling).toggle()
 //   });
 // });
 
-$(document).ready(function() {
-  $(".first-row .concept").click(function() {
-    $(this.nextElementSibling).toggle();
-  });
-  $(".second-row .concept").click(function() {
-    var answer = this.nextElementSibling;
-    var concept = this;
-    $(answer).fadeToggle();
-    $(concept).remove();
-    $(answer).click(function(){
-      $(answer).before(concept);
-      $(answer).remove();
-    });
-  });
-  $(".third-row .concept").click(function() {
-    var answer = this.nextElementSibling;
-    var concept = this;
-    $(answer).slideToggle();
-    $(".third-row .flashcard").addClass("flashcard-clicked");
-  });
-  $(".fourth-row .concept").click(function() {
-    var answer = (this).children("p");
-    alert(answer);
-  });
-  $(".fifth-row .flashcard").click(function() {
-    $(this).children("h2").toggle();
-    $(this).children("p").toggle();
-  });
-});
+// $(document).ready(function() {
+//   $(".first-row .concept").click(function() {
+//     $(this.nextElementSibling).toggle();
+//   });
+  // $(".second-row .concept").click(function() {
+  //   var answer = this.nextElementSibling;
+  //   var concept = this;
+  //   $(answer).fadeToggle();
+  //   $(concept).remove();
+  //   $(answer).click(function(){
+  //     $(answer).before(concept);
+  //     $(answer).remove();
+  // });
+
+  // $(".fourth-row .flashcard").click(function() {
+  //   var answer = $(this).children("p");
+  //   alert(answer);
+  // });
+
+//   $(".fifth-row .flashcard").click(function() {
+//     $(this).children("h2").toggle();
+//     $(this).children("p").toggle();
+//   });
+// });
